a5efa552e4896159efd66c73459bcfc632187089
OGHENEMINE CHRISTABEL EDEWOR <66080490+Chrisinabit@users.noreply.github.com>
2020-06-03 05:45:47 +0100

    Add files via upload

---
+++ Car Empire_2/js/test.js
@@ -0,0 +1,114 @@
+
+		$(document).ready(function(){
+			$("#pi").click(function(){
+				$("#myModal").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#si").click(function(){
+				$("#myModal2").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#pj").click(function(){
+				$("#myModal3").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#ri").click(function(){
+				$("#myModal4").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#ti").click(function(){
+				$("#myModal5").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#ui").click(function(){
+				$("#myModal6").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#vi").click(function(){
+				$("#myModal7").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#wi").click(function(){
+				$("#myModal8").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#xi").click(function(){
+				$("#myModal9").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#yi").click(function(){
+				$("#myModal10").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#zi").click(function(){
+				$("#myModal11").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#ai").click(function(){
+				$("#myModal12").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#bi").click(function(){
+				$("#myModal13").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#ci").click(function(){
+				$("#myModal14").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#di").click(function(){
+				$("#myModal15").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#ei").click(function(){
+				$("#myModal16").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#fi").click(function(){
+				$("#myModal17").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#gi").click(function(){
+				$("#myModal18").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#hi").click(function(){
+				$("#myModal19").modal();
+			});
+		})	;
+		$(document).ready(function(){
+			$("#ij").click(function(){
+				$("#myModal20").modal();
+			});
+		})	;
+	
+	
+	
+	
+		function myFunction(){
+			var x = document.getElementById('topnav');
+	if (x.className === "nav-pills"){
+		x.className+= "responsive";
+	}
+	else{
+		x.className = "nav-pills";
+	}
+}
